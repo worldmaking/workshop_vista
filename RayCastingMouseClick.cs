35a639f027e87ff958b8811e1362807de2ae44e0
Graham Wakefield <grrrwaaa@gmail.com>
2018-05-31 12:02:14 -0400

    new pages

---
+++ RayCastingMouseClick.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class RayCastingMouseClick : MonoBehaviour {
+
+    public Camera mainCam;
+    public int CSVFileCount = 0;
+    public SaveToCSV csv;
+    private bool isInTrial = false;
+    
+    // Update is called once per frame
+    void Update () {
+        var sourceDate = DateTime.Now;
+        //Check if space button is pressed
+        if (Input.GetKeyDown((KeyCode.Space)))
+        {
+            isInTrial = !isInTrial;
+            if (!isInTrial)
+            {
+                CSVFileCount += 1;
+                csv.FileName(sourceDate.ToString().Replace("/", "_").Replace("\\", "_").Replace(":", "_") +" " + CSVFileCount);
+                csv.Save();
+            }
+        }
+
+        if (isInTrial) 
+        { 
+            //Raycast - Cast array from the mouse click coordinate in the direction of view
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+                Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit, 100.0f))
+                {
+                    // did you pick a Stimulus, and was it changed?
+                    bool isChanged = false; 
+                    var stim = hit.transform.gameObject.GetComponent<Stimulus>();
+                    if (stim != null)
+                    {
+                        isChanged = stim.isChanged;
+                    }
+
+                    //Prints to the console the object name, the date, etc.
+				    Debug.Log("You selected the " + hit.transform.name + " at " + sourceDate + ", whose changed status is " + isChanged); 
+                    
+                    //Sending the object variables from this script to another script
+                    csv.Add(sourceDate.ToString(), hit.transform.name, hit.transform.ToString(), isChanged);
+                }
+            }
+        
+        }
+    }
+}
---
+++ SaveToCSV.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System;
+
+public class SaveToCSV : MonoBehaviour
+{
+    // Stringbuilder will concatenate many strings into lines and parapgraphs  [ https://msdn.microsoft.com/en-us/library/system.text.stringbuilder(v=vs.110).aspx ]
+    StringBuilder sb = new StringBuilder();
+    string fileTitle;
+
+    // Use this for initialization
+    void Start()
+    {
+        sb.Length = 0;
+        sb.AppendLine("Date, Hit Name, Hit Object, Is Changed");
+    }
+
+    // This will take 4 variables from another class and append them into 4 columns of data for each object
+    public void Add(string date, string hitName, string hitobj, bool isChangedBool) {
+        sb.AppendLine(String.Format("{0},{1},{2},{3}", date, hitName, hitobj, isChangedBool));
+    }
+
+    public void FileName(string fileName)
+    {
+        fileTitle = fileName;
+    }
+
+    public void Save()
+    { 
+        string filePath = getPath();
+        Debug.Log("Saved to:" + filePath);
+
+        // Writes stringbuilder to a fill     [ https://msdn.microsoft.com/en-us/library/system.io.streamwriter(v=vs.110).aspx ]
+        StreamWriter outStream = System.IO.File.CreateText(filePath);
+        outStream.WriteLine(sb);
+        outStream.Close();
+    }
+
+    // Following method is used to retrive the relative path as device platform
+    private string getPath()
+    {
+#if UNITY_EDITOR
+        return Application.dataPath +"/CSV/"+ fileTitle + ".csv";
+#elif UNITY_ANDROID
+        return Application.persistentDataPath + fileTitle + ".csv";
+#elif UNITY_IPHONE
+        return Application.persistentDataPath+"/"+ fileTitle + ".csv";
+#else
+        return Application.dataPath + "/" + fileTitle + ".csv";
+#endif
+    }
+}
